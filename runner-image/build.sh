ae42ef884f1912bccad772668043a433d53139a4
Sridevi <parimisridevi586@gmail.com>
2025-09-24 19:17:07 +1000

    added few more commands to actions

--- runner-image/build.sh
+++ runner-image/build.sh
@@ -1,5 +1,5 @@
 git pull
-sudo docker login -u AWS -p $(aws ecr get-login-password --region us-east-1) 533567530972.dkr.ecr.us-east-1.amazonaws.com/runner:latest
+# sudo docker login -u AWS -p $(aws ecr get-login-password --region us-east-1) 533567530972.dkr.ecr.us-east-1.amazonaws.com/runner:latest
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 533567530972.dkr.ecr.us-east-1.amazonaws.com
 docker build -t 533567530972.dkr.ecr.us-east-1.amazonaws.com/runner:latest .
 docker push 533567530972.dkr.ecr.us-east-1.amazonaws.com/runner:latest
--- runner-image/run.sh
+++ runner-image/run.sh
@@ -1,6 +1,5 @@
 if [ -z "${ORG}" ]; then echo Input ORG is missing ; exit 1 ; fi
 if [ -z "${TOKEN}" ]; then echo Input TOKEN is missing ; exit 1 ; fi
 if [ -z "${NAME}" ]; then echo Input NAME is missing ; exit 1 ; fi
-cd /home/github/actions-runner
 ./config.sh --unattended  --url ${ORG} --token ${TOKEN} --name ${NAME} --runnergroup Default --work _work --replace
-./run.sh #--replace
+./run.sh --replace
