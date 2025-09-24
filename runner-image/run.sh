1c955d778534606de2ee601156ab459159b02ebc
Sridevi <parimisridevi586@gmail.com>
2025-09-24 17:32:48 +1000

    added few more commands to actions

--- runner-image/run.sh
+++ runner-image/run.sh
@@ -1,5 +1,6 @@
 if [ -z "${ORG}" ]; then echo Input ORG is missing ; exit 1 ; fi
 if [ -z "${TOKEN}" ]; then echo Input TOKEN is missing ; exit 1 ; fi
 if [ -z "${NAME}" ]; then echo Input NAME is missing ; exit 1 ; fi
-./config.sh --unattended  -v /var/run/docker.sock:/var/run/docker.sock --group-add $(stat -c '%g' /var/run/docker.sock) -e DOCKER_GROUP=$(stat -c '%g' /var/run/docker.sock) --url ${ORG} --token ${TOKEN} --name ${NAME} --runnergroup Default --work _work --replace
+cd /home/github/actions-runner
+./config.sh --unattended  --url ${ORG} --token ${TOKEN} --name ${NAME} --runnergroup Default --work _work --replace
 ./run.sh #--replace
